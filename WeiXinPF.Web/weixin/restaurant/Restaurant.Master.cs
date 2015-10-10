748b7a7d14f6472638165a47c77657b30a338fd3
xqfgbc_cp <SND\xqfgbc_cp>
2015-10-10 09:46:11 +0000

    使用MasterPage替换购物车界面的Header和Footer

---
+++ WeiXinPF.Web/weixin/restaurant/Restaurant.Master.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WeiXinPF.Web.weixin.restaurant
+{
+    using WeiXinComm;
+    public partial class Restaurant : System.Web.UI.MasterPage
+    {
+        public string title { get; set; }
+        public int shopid { get; set; }
+        public string openid { get; set; }
+        public string hotelName { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.shopid = string.IsNullOrEmpty(WebHelper.GetQueryString("shopid")) ? 0 : int.Parse(WebHelper.GetQueryString("shopid"));
+            this.openid = string.IsNullOrEmpty(WebHelper.GetQueryString("openid")) ? "loseopenid" : WebHelper.GetQueryString("openid");
+            this.title = this.GetTitle();
+
+            if (!Page.IsPostBack)
+            {
+                if (this.shopid == 0)
+                {
+                    this.hotelName = "无选定商铺";
+                }
+                else
+                {
+                    this.hotelName = new BLL.wx_diancai_shopinfo().GetModel(this.shopid).hotelName;
+                }                
+            }
+        }
+
+        private string GetTitle()
+        {
+            var pageName = WebHelper.GetPageName();
+
+            switch (pageName)
+            {
+                case "caidan_guanyu.aspx":
+                    return "关于";
+                case "index.aspx":
+                    return "美食选购";
+                case "diancai_shoppingCart.aspx":
+                    return "商铺购物车";
+                default:
+                    return "欢迎光临";
+            }
+        }
+    }
+}
---
+++ WeiXinPF.WeiXinComm/WebHelper.cs
@@ -0,0 +1,82 @@
+﻿namespace WeiXinPF.WeiXinComm
+{
+    using System;
+    using System.Linq;
+    using System.Web;
+
+    public class WebHelper
+	{   
+		/// <summary>
+		/// 转向
+		/// </summary>
+		/// <param name="url">转向的url</param>
+		public static void RedirectUrl(string url)
+		{
+			HttpContext.Current.Response.Redirect(MapUrl(url));
+		}
+
+		public static void RedirectUrlWithOutHostPort(string url)
+		{
+			HttpContext.Current.Response.Redirect(url);
+		}
+
+		/// <summary>
+		/// 得到站点地址
+		/// </summary>
+		/// <returns></returns>
+		public static string GetHostPort()
+		{
+			if (HttpContext.Current.Request.Url.Port == 80)
+				return string.Format("http://{0}{1}/",
+				HttpContext.Current.Request.Url.Host,
+				HttpContext.Current.Request.ApplicationPath.TrimEnd('/'));
+			else
+				return string.Format("http://{0}:{1}{2}/",
+					HttpContext.Current.Request.Url.Host,
+					HttpContext.Current.Request.Url.Port,
+					HttpContext.Current.Request.ApplicationPath.TrimEnd('/'));
+		}
+
+		/// <summary>
+		/// 得到完整路径
+		/// </summary>
+		/// <param name="url">相对url</param>
+		/// <returns></returns>
+		public static string MapUrl(string url)
+		{
+			return string.Format("{0}{1}", GetHostPort(), url);
+		}
+
+		/// <summary>
+		/// 得到完整物理路径
+		/// </summary>
+		/// <returns></returns>
+		public static string GetBaseDirectory()
+		{
+			return string.Format(@"{0}", AppDomain.CurrentDomain.BaseDirectory);
+		}
+
+		/// <summary>
+		/// 得到物理文件路径
+		/// </summary>
+		/// <param name="path">相对地址</param>
+		/// <returns></returns>
+		public static string MapPath(string path)
+		{
+			return string.Format(@"{0}{1}", GetBaseDirectory(), path);
+		}
+
+		public static string GetQueryString(string key)
+		{
+			if (HttpContext.Current.Request.QueryString[key] == null)
+			{ return string.Empty; }
+			else
+			{ return HttpContext.Current.Request.QueryString[key]; }
+		}
+
+        public static string GetPageName()
+        {
+            return HttpContext.Current.Request.Path.Split('/').Last();
+        }
+	}
+}
