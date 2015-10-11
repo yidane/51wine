b4a276029e603b39abd169ce7ad8d52163aa9f4d
xqfgbc_cp <SND\xqfgbc_cp>
2015-10-11 08:23:39 +0000

    修改点菜购物车相关前台页面

--- WeiXinPF.Web/weixin/restaurant/index.aspx.cs
+++ WeiXinPF.Web/weixin/restaurant/index.aspx.cs
@@ -109,7 +109,7 @@ namespace WeiXinPF.Web.weixin.restaurant
                 for (int j = 0; j < manage1.Tables[0].Rows.Count; j++)
                 {
                     manageString += "<dd> ";
-                    manageString += "<span class=\"count_zero\" id=\"num_" + manage1.Tables[0].Rows[j]["id"].ToString() + "_1\" onClick=\"addProduct('" + manage1.Tables[0].Rows[j]["id"].ToString() + "','1','" + manage1.Tables[0].Rows[j]["cpName"].ToString() + "','18.0','" + categoryid + "',1);\">0</span>";
+                    manageString += "<span class=\"count_zero\" id=\"num_" + manage1.Tables[0].Rows[j]["id"].ToString() + "_1\" onClick=\"addProduct('" + manage1.Tables[0].Rows[j]["id"].ToString() + "','1','" + manage1.Tables[0].Rows[j]["cpName"].ToString() + "','" + manage1.Tables[0].Rows[j]["zkPrice"].ToString() + "','" + categoryid + "',1);\">0</span>";
                     manageString += " <div class=\"tupian\"><img src=" + manage1.Tables[0].Rows[j]["picUrl"].ToString() + " onClick=\"htmlit('" + manage1.Tables[0].Rows[j]["picUrl"].ToString() + "','" + manage1.Tables[0].Rows[j]["cpName"].ToString() + "'," + manage1.Tables[0].Rows[j]["id"].ToString() + ")\">";
                     manageString += "<a href=\"javascript:addProduct('" + manage1.Tables[0].Rows[j]["id"].ToString() + "','1','" + manage1.Tables[0].Rows[j]["cpName"].ToString() + "','" + manage1.Tables[0].Rows[j]["zkPrice"].ToString() + "','" + categoryid + "',1);\" class=\"add\" data-foodid=\"" + manage1.Tables[0].Rows[j]["id"].ToString() + "_1\">";
                     manageString += "<h3>" + manage1.Tables[0].Rows[j]["cpName"].ToString() + "</h3>";
@@ -121,60 +121,7 @@ namespace WeiXinPF.Web.weixin.restaurant
 
             }
 
-           // BindCaiPing(category1);
-
         }
 
-        /// <summary>
-        /// 绑定右边的菜品信息
-        /// </summary>
-        //protected void BindCaiPing(DataSet category)
-        //{
-        //    if (category.Tables[0].Rows.Count > 0)//分类
-        //    {
-        //        manageString = "";
-        //        cateString = "";
-        //        categories = "{";
-        //        for (int i = 0; i < category.Tables[0].Rows.Count; i++)
-        //        {
-        //            if (i == 0)
-        //            {
-        //                cateString += "<dd class=\"active\"><a style=\"width: 100%;\" href=\"index.aspx?categoryid=" + category.Tables[0].Rows[i]["id"].ToString() + "&openid=" + openid + "&shopid=" + shopid + "\">" + category.Tables[0].Rows[i]["categoryName"].ToString() + "</a></dd>";
-
-        //                DataSet manage = managebll.GetList(Convert.ToInt32(category.Tables[0].Rows[i]["id"].ToString()));//分类id
-        //                if (manage.Tables[0].Rows.Count > 0)//菜品
-        //                {
-        //                    for (int j = 0; j < manage.Tables[0].Rows.Count; j++)
-        //                    {
-        //                        manageString += "<dd> ";
-        //                        manageString += "<span class=\"count_zero\" id=\"num_" + manage.Tables[0].Rows[j]["id"].ToString() + "_1\" onClick=\"addProduct('" + manage.Tables[0].Rows[j]["id"].ToString() + "','1','" + manage.Tables[0].Rows[j]["cpName"].ToString() + "','18.0','" + category.Tables[0].Rows[i]["id"].ToString() + "',1);\">0</span>";
-        //                        manageString += " <div class=\"tupian\"><img src=" + manage.Tables[0].Rows[j]["picUrl"].ToString() + " onClick=\"htmlit('http:','" + manage.Tables[0].Rows[j]["cpName"].ToString() + "'," + manage.Tables[0].Rows[j]["id"].ToString() + ")\">";
-        //                        manageString += "<a href=\"javascript:addProduct('" + manage.Tables[0].Rows[j]["id"].ToString() + "','1','" + manage.Tables[0].Rows[j]["cpName"].ToString() + "','" + manage.Tables[0].Rows[j]["zkPrice"].ToString() + "','" + category.Tables[0].Rows[i]["id"].ToString() + "',1);\" class=\"add\" data-foodid=\"" + manage.Tables[0].Rows[j]["id"].ToString() + "_1\">";
-        //                        manageString += "<h3>" + manage.Tables[0].Rows[j]["cpName"].ToString() + "</h3>";
-        //                        manageString += " <em>" + manage.Tables[0].Rows[j]["zkPrice"].ToString() + "元/件<del>" + manage.Tables[0].Rows[j]["cpPrice"].ToString() + "元/件</del></em>";
-        //                        manageString += "  <p class=\"dpNum\">" + manage.Tables[0].Rows[j]["scan"].ToString() + "</p></a> ";
-        //                        manageString += " <a href=\"javascript:reduceProduct('" + manage.Tables[0].Rows[j]["id"].ToString() + "','1',1);\" class=\"reduce\" id=\"del_" + manage.Tables[0].Rows[j]["id"].ToString() + "_1\" style=\"display:none;\"><b class=\"ico_reduce\">减一份</b></a>";
-        //                        manageString += " </div></dd>";
-        //                    }
-
-        //                }
-
-        //            }
-        //            else
-        //            {
-        //                cateString += "<dd ><a style=\"width: 100%;\" href=\"index.aspx?categoryid=" + category.Tables[0].Rows[i]["id"] + "&openid=" + openid + "&shopid=" + shopid + "\">" + category.Tables[0].Rows[i]["categoryName"] + "</a></dd>";
-        //            }
-
-        //            categories += "\"" + category.Tables[0].Rows[i]["id"].ToString() + "\"" + ":" + "\"" + category.Tables[0].Rows[i]["categoryName"].ToString() + "\"" + ",";
-
-        //        }
-
-        //        categories = categories.Substring(0, categories.Length - 1);
-
-        //        categories += "}";
-        //    }
-
-
-        //}
     }
 }
