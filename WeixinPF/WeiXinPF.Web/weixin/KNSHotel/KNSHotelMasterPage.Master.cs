a505e7c0dc55b1f1436325b1e916f0657fcd9568
xqfgbc_cp <SND\xqfgbc_cp>
2015-10-16 10:05:50 +0000

    新建喀纳斯酒店功能

---
+++ WeixinPF/WeiXinPF.Web/weixin/KNSHotel/KNSHotelMasterPage.Master.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WeiXinPF.Web.weixin.KNSHotel
+{
+    using System.Data;
+
+    using WeiXinPF.Common;
+    using WeiXinPF.WeiXinComm;
+
+    public partial class KNSHotelMasterPage : System.Web.UI.MasterPage
+    {
+        public int hotelid { get; set; }
+        public string openid { get; set; }
+        public string title { get; set; }
+
+        public int wid { get; set; }
+
+        public int dingdannum { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            hotelid = MyCommFun.RequestInt("hotelid");
+            openid = MyCommFun.QueryString("openid");
+            this.title = this.GetTitle();
+
+            if (!Page.IsPostBack)
+            {
+                if (!string.IsNullOrEmpty(this.openid) && this.hotelid >0)
+                {
+                    var dingdanbll = new BLL.wx_hotel_dingdan();
+                    var hotelInfo = new BLL.wx_hotels_info().GetModel(this.hotelid);
+
+                    if (hotelInfo != null)
+                    {
+                        this.wid = hotelInfo.wid.Value;
+                    }
+
+                    DataSet dr = dingdanbll.GetList(this.openid, this.hotelid);
+                    if (dr.Tables[0].Rows.Count > 0)
+                    {
+                        this.dingdannum = dr.Tables[0].Rows.Count;
+                    }
+                    else
+                    {
+                        this.dingdannum = 0;
+                    } 
+                }
+                               
+            }
+        }
+
+        private string GetTitle()
+        {
+            var pageName = WebHelper.GetPageName();
+
+            switch (pageName)
+            {
+                case "hotel_detail.aspx":
+                    return "关于";
+                case "index.aspx":
+                    return "房型选择";
+                case "hotel_order.aspx":
+                    return "订单";
+                default:
+                    return "订单";
+            }
+        }        
+    }
+}
---
+++ WeixinPF/WeiXinPF.Web/weixin/KNSHotel/index.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="KNSHotelMasterPage.Master" CodeBehind="index.aspx.cs" Inherits="WeiXinPF.Web.weixin.KNSHotel.index" %>
+
+
+<asp:Content ID="h" ContentPlaceHolderID="head" runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>在线预订</title>
+<meta name="viewport" content="width=device-width,height=device-height,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="format-detection" content="telephone=no">
+<link href="css/hotels.css" rel="stylesheet" type="text/css">
+</asp:Content>
+
+
+<asp:Content ID="c" ContentPlaceHolderID="content" runat="server" class="mode_webapp">
+<div class="qiandaobanner">  <a href="hotel_order_onlin.aspx?openid=<%=openid %>" > <img   src="<%=image %>"  /></a> </div>
+<div class="cardexplain"> 
+
+  <!--普通用户登录时显示-->
+<%=numdingdan %>  
+
+<!--商家房价及类型-->
+<ul class="round">
+<%=yuding %>       
+</ul>
+
+<!--后台可控制是否显示-->
+<div class="detailcontent"><h2>订单说明</h2>
+  <div class="content">
+  <span style="color:#444444;font-family:'Microsoft YaHei', Helvitica, Verdana, Arial, san-serif;font-size:14px;font-weight:bold;line-height:21px;background-color:#FCFCFC;"><%=shuoming %></span></div>
+  </div>
+   
+<!--后台可控制是否显示-->
+<ul class="round">
+<li class="addr"><a href="http://api.map.baidu.com/marker?location=<%=xplace %>,<%=yplace %>&amp;title=<%=dizhi %>&amp;content=<%=dizhi %>&amp;output=html"><span><%=dizhi %></span></a></li>
+<li class="tel"><a href="tel:<%=tel %>"><span><%=tel %> 电话预订</span></a></li>
+<li class="detail"><a href="hotel_detail.aspx?openid=<%=openid%>&hotelid=<%=hotelid %>"><span>查看商家详情</span></a></li>
+</ul>
+</div>
+<script src="js/plugback.js" type="text/javascript" ></script>
+<script type="text/javascript">
+    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+        WeixinJSBridge.call('hideToolbar');
+    });
+</script>
+
+</asp:Content>
+
