ea241e158198f0d24ea6eb7799f31b15873b171f
xqfgbc_cp <SND\xqfgbc_cp>
2015-10-26 06:57:15 +0000

    修改酒店验证码功能

--- WeiXinPF.Infrastructure.DomainDataAccess/IdentifyingCode/DTO/OrderDetailDTO.cs
+++ WeiXinPF.Infrastructure.DomainDataAccess/IdentifyingCode/DTO/OrderDetailDTO.cs
@@ -17,6 +17,16 @@ namespace WeiXinPF.Infrastructure.DomainDataAccess.IdentifyingCode.DTO
 
         public string CustomerName { get; set; }
 
+        public int Number { get; set; }
+
+        public string ProductName { get; set; }
+
+        public DateTime ArriveTime { get; set; }
+
+        public DateTime LeaveTime { get; set; }
+
+        public double Price { get; set; }
+
         public double PayAmount { get; set; }
 
         public DateTime ModifyTime { get; set; }
